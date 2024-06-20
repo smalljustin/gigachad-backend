c086b3dbb22e3a73f35ad8f9e873d1cc2cef3f7e
Justin Schmitz <schmitzjustin2000@gmail.com>
2024-06-20 10:31:51 -0500

    Add minification methods

--- src/main/java/sgtbigbird/gcc/backend/controller/OutController.java
+++ src/main/java/sgtbigbird/gcc/backend/controller/OutController.java
@@ -21,6 +21,7 @@ import sgtbigbird.gcc.backend.service.PublishingService;
 
 import javax.naming.AuthenticationException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/out")
@@ -36,21 +37,37 @@ public class OutController {
     @Autowired
     ObjectMapper objectMapper;
 
-    @GetMapping("/data/runkey")
-    public List<DataPoint> getDataPointsByRunkey(@RequestParam String runkey) throws MapTagNotFoundException {
+    @GetMapping("/data/runkey/verbose")
+    public List<DataPoint> getDataPointsByRunkey_V(@RequestParam String runkey) throws MapTagNotFoundException {
         return publishingService.getDataPointsByRunkey(runkey);
     }
 
-    @GetMapping("/data/tag")
-    public List<DataPoint> getDataPointsByTag(@RequestParam String tag) throws MapTagNotFoundException {
+    @GetMapping("/data/tag/verbose")
+    public List<DataPoint> getDataPointsByTag_V(@RequestParam String tag) throws MapTagNotFoundException {
         return publishingService.getDataPointsByTag(tag);
     }
 
-    @GetMapping("/all")
-    public List<DataPoint> getAllDataPoints() {
-        return publishingService.getAllDataPoints();
+    @GetMapping("/data/runkey")
+    public List<DataPointSmall> getDataPointsByRunkey(@RequestParam String runkey) throws MapTagNotFoundException {
+        return publishingService.getDataPointsByRunkey(runkey)
+                .stream().map((DataPointSmall::new)).collect(Collectors.toList());
     }
 
+    @GetMapping("/data/tag")
+    public List<DataPointSmall> getDataPointsByTag(@RequestParam String tag) throws MapTagNotFoundException {
+        return publishingService.getDataPointsByTag(tag)
+                .stream().map((DataPointSmall::new)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/all/verbose")
+    public List<DataPoint> getAllDataPoints_V() {
+        return publishingService.getAllDataPoints();
+    }
+    @GetMapping("/all")
+    public List<DataPointSmall> getAllDataPoints() {
+        return publishingService.getAllDataPoints()
+                .stream().map((DataPointSmall::new)).collect(Collectors.toList());
+    }
     @GetMapping("/maptag")
     public List<MapTag> getRunTags() throws AuthenticationException, JsonProcessingException {
         List<MapTag> ret = mapTagRepository.findAll();
---
+++ src/main/java/sgtbigbird/gcc/backend/model/DataPointSmall.java
@@ -0,0 +1,51 @@
+package sgtbigbird.gcc.backend.model;
+
+import lombok.Data;
+
+@Data
+public class DataPointSmall {
+    public DataPointSmall(DataPoint dp) {
+        this.pidx = dp.getPidx();
+        this.runId = dp.getRunId();
+        this.respawnId = dp.getRespawnId();
+        this.username = dp.getUsername();
+        this.version = dp.getVersion();
+        this.vehicleType = dp.getVehicleType();
+        this.speed = dp.getSpeed();
+        this.frontSpeed = dp.getFrontSpeed();
+        this.inputSteer = dp.getInputSteer();
+        this.inputBrake = dp.isInputBrake();
+        this.inputGas = dp.getInputGas();
+        this.slipDir = dp.getSlipDir();
+        this.slipLeft = dp.getSlipLeft();
+        this.curGear = dp.getCurGear();
+        this.flGroundContactMaterial = dp.getFlGroundContactMaterial();
+        this.frGroundContactMaterial = dp.getFrGroundContactMaterial();
+        this.rlGroundContactMaterial = dp.getRlGroundContactMaterial();
+        this.rrGroundContactMaterial = dp.getRrGroundContactMaterial();
+        this.reactor = dp.getReactor();
+        this.time = dp.getTime();
+        this.dt = dp.getDt();
+    }
+    private int pidx;
+    private String runId;
+    private String respawnId;
+    private String username;
+    private int version;
+    private String vehicleType;
+    private float speed;
+    private float frontSpeed;
+    private float inputSteer;
+    private boolean inputBrake;
+    private float inputGas;
+    private float slipDir;
+    private float slipLeft;
+    private int curGear;
+    private String flGroundContactMaterial;
+    private String frGroundContactMaterial;
+    private String rlGroundContactMaterial;
+    private String rrGroundContactMaterial;
+    private String reactor;
+    private long time;
+    private float dt;
+}
